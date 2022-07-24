451050269986861c729a9ffced65907389a8b2a2
swapnil rathod <rathodswapnil9@gmail.com>
2022-07-24 22:45:13 +0530

    first commit by swapnil

---
+++ src/main/java/com/example/springapis/SpringapisApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springapis/SpringapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
